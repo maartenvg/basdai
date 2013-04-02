b570901cc3a6605e06802eb41190c9ae0ba99a77
maartenvg <git@maartenvangrootel.nl>
2013-04-02 15:55:22 -0400

    Initial commit of BASDAI app

---
+++ README.md
@@ -0,0 +1,4 @@
+BASDAI APP
+==========
+
+The BASDAI App provides an easy to fill out BASDAI form and gives you to opportunity to easily see your personal progress over time and print or e-mail a PDF of your results. Besides that, you can share your answers with your doctor(s) in a secure way.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasdaiApp::Application.config.secret_token = '669e8294ade63cf864a13c95afc9157adb22e18660f8db6ad7108553315190d9364d0a4cefbd5f648ce70935d3225ff1f0c94b5fde122d44d66b000a2058e382'

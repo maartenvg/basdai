4e0d965c11f6611354da0531ce9b3f1c087ecd84
maartenvg <git@maartenvangrootel.nl>
2013-04-08 15:25:38 -0400

    Update views for questionnaires

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -3,8 +3,13 @@ class Questionnaire < ActiveRecord::Base
   validates_inclusion_of :medication_effectiveness, in: 1..10, allow_nil: true
   validates_inclusion_of :as_pain, :fatigue, :localized_discomfort, :other_pain, :wakeup_discomfort, in: 1..10
   validates_inclusion_of :morning_stiffness, in: 0..10
-  
   def to_s
     "Questionnaire no. #{@id}"
+  end
+
+  def score
+    overall = fatigue + as_pain + other_pain + localized_discomfort;
+    mornings = wakeup_discomfort + morning_stiffness;
+    (overall + ( mornings / 2 ) ) * 0.2;
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,22 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :birthdate, :gender, :password, :password_confirmation
-  
+
   validates_uniqueness_of :name
   validates_presence_of :email, :birthdate
   validates_inclusion_of :gender, in: ['m', 'f']
-  
-  has_secure_password
 
-  def to_s 
-    @name
-  end
+  has_secure_password
   
+  def to_s
+    name
+  end
+
+  def print_gender
+    gender == 'm'  ? 'Male' : 'Female'
+  end
+
   def age
-    now = Time.now.to_date
-    now.year - @birthdate.year - ((now.month > @birthdate.month || (now.month == @birthdate.month && now.day >= @birthdate.day)) ? 0 : 1)
+    now = Time.now
+    now.year - birthdate.year - ((now.month > birthdate.month || (now.month == birthdate.month && now.day >= birthdate.day)) ? 0 : 1)
   end
 end

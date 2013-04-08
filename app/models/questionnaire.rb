a0c587e961cff76699b5eced6f3f8d03fa216a89
maartenvg <git@maartenvangrootel.nl>
2013-04-08 08:44:58 -0400

    Add validations

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,3 +1,6 @@
 class Questionnaire < ActiveRecord::Base
   attr_accessible :age, :as_pain, :fatigue, :gender, :localized_discomfort, :medication, :medication_effectiveness, :morning_stiffness, :other_pain, :wakeup_discomfort
+  validates_numericality_of :age, greater_than_or_equal_to: 0
+  validates_numericality_of :medication_effectiveness, greater_than: 0, less_than: 11, allow_nil: true
+  validates_numericality_of :as_pain, :fatigue, :gender, :localized_discomfort, :morning_stiffness, :other_pain, :wakeup_discomfort, greater_than: 0, less_than: 11 
 end

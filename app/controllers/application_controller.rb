ec5ddfb15877011c4f032047453bed18a0c0e7b0
maartenvg <git@maartenvangrootel.nl>
2013-04-08 13:18:14 -0400

    Add authorization + fix tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :authorize
+  
+  
+  private
+  
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
+  end
+  
+  def active_user?
+    !session[:user_id].nil?
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  skip_before_filter :authorize
+  
   def index
+    @logged_in = active_user?
   end
 end
